18c507f62030a73213c6768f190a87cbb986b6b5
Antoine H <ah.antix@gmail.com>
2023-07-26 14:46:57 +0200

    add comments on app.js

--- app.js
+++ app.js
@@ -8,12 +8,14 @@ const sauceRoutes = require('./routes/sauce');
 
 const app = express();
 
+// Connexion à BDD
 mongoose.connect( process.env.MONGODB,
   { useNewUrlParser: true,
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
-  
+
+// Gestion des CORS
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
